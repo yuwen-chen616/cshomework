6e6580afc83a50e39db491b546ccf053f68a43ab
yuwen-chen616 <57294551+yuwen-chen616@users.noreply.github.com>
2020-05-21 22:21:06 +0800

    Add files via upload
    1. 写一个控制台程序，实现计算器功能。输入两个数字和一个运算符，打印出计算结果。
    2. 写一个Windows窗体应用，实现计算器功能。在两个文本框中输入数字，在组合框中选择运算符，点计算按钮后显示出计算结果。

---
+++ cshomework1/csHomework1_Calculator2/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace csHomework1_Calculator2
+{
+    class Program
+    {
+        static int TobeInt()
+        {
+            string str = Console.ReadLine();
+            bool IsInt;//判断输入的字符是否为整数
+            int num;
+            IsInt = int.TryParse(str, out num);
+            while (!IsInt)
+            {
+                Console.WriteLine("输入的字符不是整数，请重新输入！");
+            }
+            return num;
+        }
+        static void Main(string[] args)
+        {
+            int a, b,opt;
+            Console.WriteLine("输入第一个数字：");
+            a = TobeInt();
+            Console.WriteLine("输入第二个数字：");
+            b = TobeInt();
+            Console.WriteLine("请选择运算符：");
+            opt = TobeInt();
+            while(opt<1||opt>4)
+            {
+                Console.WriteLine("输入的运算符不存在，请重新输入！");
+            }
+            switch(opt)
+            {
+                case 1:
+                    Console.WriteLine("两个数的和为：{0}", a + b);
+                    break;
+                case 2:
+                    Console.WriteLine("两个数的差为：{0}", a - b);
+                    break;
+                case 3:
+                    Console.WriteLine("两个数的乘积为：{0}", a * b);
+                    break;
+                case 4:
+                    if(b==0)
+                    {
+                        Console.WriteLine("错误！");
+                    }
+                    else
+                    {
+                        Console.WriteLine("两个数的商为：{0}", a / b);
+                    }
+                    break;
+            }   
+        }
+        
+    }
+}
---
+++ cshomework1/csHomework1_CalculatorWin/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace csHomework1_CalculatorWin
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a, b;
+            string s = textBox1.Text;
+            bool IsInt;//isEft表示文本框中输入字符是否能成功解析为数字，解析成功则isEft为true，反之为false。
+            IsInt = int.TryParse(s, out a);//解析第一个操作数
+            if (!IsInt)//第一个操作数解析失败
+            {
+                label1.Text = "第一个操作数必须是数字";
+                return;
+            }
+            s = textBox2.Text;
+            IsInt = int.TryParse(s, out b);//解析第二个操作数
+            if (!IsInt)//第二个操作数解析失败
+            {
+                label2.Text = "第二个操作数必须是数字";
+                return;
+            }
+            int opt = comboBox1.SelectedIndex;
+            
+            switch (opt)
+            {
+                case 1:
+                    textBox3.Text = $"{a + b}";
+                    break;
+                case 2:
+                    textBox3.Text = $"{a - b}";
+                    break;
+                case 3:
+                    textBox3.Text = $"{a * b}";
+                    break;
+                case 4:
+                    if (b == 0)
+                    {
+                        label2.Text = "错误！除数不能为0";
+                    }
+                    else
+                    {
+                        textBox3.Text = $"{a / b}";
+                    }
+                    break;
+            }
+            return;
+        }
+    }
+}
