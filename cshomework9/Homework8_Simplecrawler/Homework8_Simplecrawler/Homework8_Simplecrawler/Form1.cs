b9cba4f92859e79414c8680358b86d46287d073c
yuwen-chen616 <57294551+yuwen-chen616@users.noreply.github.com>
2020-05-21 22:29:31 +0800

    Add files via upload
    @全体成员   周五的课堂作业有一定难度，大家有时间可以提早做一下。
    为订单管理的程序添加一个WinForm的界面。通过这个界面，调用OrderService的各个方法，实现创建订单、删除订单、修改订单、查询订单、导出订单、导入订单等功能。
    
    要求：
    （1）WinForm的界面部分单独写一个项目，依赖于原来的项目。
    （2）可以使用两个窗口。主窗口实现查询展示功能，以及放置各种功能按钮。新建/修改订单功能使用弹出

---
+++ cshomework9/Homework8_Simplecrawler/Homework8_Simplecrawler/Homework8_Simplecrawler/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Homework8_Simplecrawler
+{
+    public partial class Form1 : Form
+    {
+        BindingSource BS = new BindingSource();
+        SimpleCrawler crawler = new SimpleCrawler();
+        Thread thread = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+            urlresult.DataSource = BS; ;
+            crawler.PageDownloaded += Crawler_PageDownloaded;
+            crawler.CrawlerStopped += Crawler_CrawlerStopped;
+        }
+
+        private void Crawler_CrawlerStopped(SimpleCrawler obj)
+        {
+            Action action = () => lblInfo.Text = "爬虫已停止";
+            if (this.InvokeRequired)
+            {
+                this.Invoke(action);
+            }else
+            {
+                action();
+            }
+    }
+
+        private void Crawler_PageDownloaded(SimpleCrawler crawler, string url, string info) {
+            var pageInfo = new { Index = BS.Count + 1, URL = url, Status = info };
+            Action action = () => { BS.Add(pageInfo); };
+            if (this.InvokeRequired)
+            {
+                this.Invoke(action);
+            }else
+            {
+                action();
+            }
+        }
+
+
+        private void btnStart_Click(object sender, EventArgs e) {
+            BS.Clear();
+            if (thread != null)
+            {
+                thread.Abort();
+            }
+            thread = new Thread(crawler.Start);
+            thread.Start();
+            lblInfo.Text = "爬虫已启动";
+        }
+
+        private void txtUrl_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ cshomework9/Homework8_Simplecrawler/Homework8_Simplecrawler/Homework8_Simplecrawler/SimpleCrawler.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//改进书上例子9-10的爬虫程序。
+// （1）只爬取起始网站上的网页 
+//（2）只有当爬取的是html文本时，才解析并爬取下一级URL。
+// （3）相对地址转成绝对地址进行爬取。
+//（4）尝试使用Winform来配置初始URL，启动爬虫，显示已经爬取的URL和错误的URL信息。
+
+namespace Homework8_Simplecrawler
+{
+    internal class SimpleCrawler
+    {
+        public event Action<SimpleCrawler> CrawlerStopped;
+        public event Action<SimpleCrawler, string, string> PageDownloaded;
+        public string StartURL = "https://www.cnblogs.com/dstang2000/"; //起始网页
+        //所有已下载和待下载URL，key是URL，value表示是否下载成功
+        private Dictionary<string, bool> urls = new Dictionary<string, bool>();
+        private Queue<string> queuepend = new Queue<string>();//待下载队列
+
+        public void Start()
+        {
+            urls.Clear();
+            queuepend.Clear();
+            queuepend.Enqueue(StartURL);
+
+            while (urls.Count < 100 && queuepend.Count > 0)
+            {
+                string url = queuepend.Dequeue();
+                try
+                {
+                    string html = DownLoad(url);
+                    urls[url] = true;
+                    PageDownloaded(this, url, "success");
+                    Parse(html, url);//解析,并加入新的链接
+                }
+                catch (Exception ex)
+                {
+                    PageDownloaded(this, url, "  Error:" + ex.Message); //显示已经爬取的URL和错误的URL信息。
+                }
+            }
+            CrawlerStopped(this);
+        }
+        
+        private string DownLoad(string url)
+        {
+            WebClient webClient = new WebClient();
+            webClient.Encoding = Encoding.UTF8;
+            string html = webClient.DownloadString(url);
+            string fileName = urls.Count.ToString();
+            File.WriteAllText(fileName, html, Encoding.UTF8);
+            return html;
+        }
+
+        //只有当爬取的是html文本时，才解析并爬取下一级URL。
+        private void Parse(string html, string pageUrl)
+        {
+            string urlquery = @"(href|HREF)\s*=\s*[""'](?<url>[^""'#>]+)[""']";
+            var matches = new Regex(urlquery).Matches(html);
+            foreach (Match match in matches)
+            {
+                string URL = match.Groups["url"].Value;
+                if (URL == null || URL == "") continue;
+                URL = TransferUrl(URL, pageUrl);//转绝对路径
+
+                //解析出host和file两个部分，进行过滤
+                string urlParse = @"^(?<site>https?://(?<host>[\w\d.]+)(:\d+)?($|/))([\w\d]+/)*(?<file>[^#?]*)";
+                Match UrlMatch = Regex.Match(URL, urlParse);
+                string host = UrlMatch.Groups["host"].Value;
+                string file = UrlMatch.Groups["file"].Value;
+                if (file == "") file = "index.html";
+                string HostFilter = "^" + host + "$";
+                if (Regex.IsMatch(host, HostFilter) && Regex.IsMatch(file, ".html?$")
+                  && !urls.ContainsKey(URL))
+                {
+                    queuepend.Enqueue(URL);
+                    urls.Add(URL, false);
+                }
+            }
+        }
+
+        //相对路径转为绝对路径
+        private string TransferUrl(string url, string pageUrl)
+        {
+            if (url.Contains("://"))
+            {
+                return url;
+            }
+            if (url.StartsWith("//"))
+            {
+                return "http:" + url;
+            }
+            if (url.StartsWith("/"))
+            {
+                string urlParse = @"^(?<site>https?://(?<host>[\w\d.]+)(:\d+)?($|/))([\w\d]+/)*(?<file>[^#?]*)";
+                Match urlMatch = Regex.Match(pageUrl, urlParse);
+                String site = urlMatch.Groups["site"].Value;
+                return site.EndsWith("/") ? site + url.Substring(1) : site + url;
+            }
+            if (url.StartsWith("../"))
+            {
+                url = url.Substring(3);
+                int idx = pageUrl.LastIndexOf('/');
+                return TransferUrl(url, pageUrl.Substring(0, idx));
+            }
+            if (url.StartsWith("./"))
+            {
+                return TransferUrl(url.Substring(2), pageUrl);
+            }
+            int end = pageUrl.LastIndexOf("/");
+            return pageUrl.Substring(0, end) + "/" + url;
+        }
+    }
+}
