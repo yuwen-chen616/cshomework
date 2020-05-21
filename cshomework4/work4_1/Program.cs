1f83e068a0cb8ccfde307e693121b6694072dc0c
yuwen-chen616 <57294551+yuwen-chen616@users.noreply.github.com>
2020-05-21 22:22:31 +0800

    Add files via upload
    1、为示例中的泛型链表类添加类似于List<T>类的ForEach(Action<T> action)方法。通过调用这个方法打印链表元素，求最大值、最小值和求和（使用lambda表达式实现）。
    
    2、使用事件机制，模拟实现一个闹钟功能。闹钟可以有嘀嗒（Tick）事件和响铃（Alarm）两个事件。在闹钟走时时或者响铃时，在控制台显示提示信息。

---
+++ cshomework4/work4_1/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace work4_1
+{
+    public class Node<T>
+    {
+        public Node<T> Next { get; set; }
+        public T Data { get; set; }
+
+        public Node(T t)
+        {
+            Next = null;
+            Data = t;
+        }
+    }
+
+    //泛型链表类
+    public class GenericList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+
+        public GenericList()
+        {
+            tail = head = null;
+        }
+
+        public Node<T> Head
+        {
+            get => head;
+        }
+
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if (tail == null)
+            {
+                head = tail = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        }
+        public void ForEach(Action<T> action)   // foreach 函数
+        {
+            for (Node<T> node = head;
+             node != null; node = node.Next)
+            {
+                action(node.Data);
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            int max = 0;
+            int min = 0;
+            GenericList<int> intList = new GenericList<int>();
+            for (int i = 0; i < 10; i++)//初始化链表
+            {
+                intList.Add(i);
+            }
+            intList.ForEach(num => Console.Write(num + " ")); // 遍历输出
+            intList.ForEach(num => sum += num); //计算和
+            intList.ForEach(num => max = max > num ? max : num);// 计算最大值
+            intList.ForEach(num => min = min < num ? min : num);// 计算最小值
+            Console.WriteLine("\n" + "the sum is :" + sum); //输出
+            Console.WriteLine("the max is :" + max);
+            Console.WriteLine("the min is :" + min);
+        }
+    }
+}
---
+++ cshomework4/work4_2/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace work4_2
+{
+    class Program
+    {
+        public delegate void AlmClkHandler(object sender);
+
+        class AlarmClock
+        {
+            //定义滴答（OnTick）事件和响铃（OnAlarm）事件
+            public event AlmClkHandler OnTick;
+            public event AlmClkHandler OnAlarm;
+
+            public void Tick()
+            {
+                //触发OnTick事件
+                OnTick(this);
+            }
+
+            public void Alarm()
+            {
+                //触发OnAlarm事件
+                OnAlarm(this);
+            }
+        }
+
+        class Form
+        {
+            public AlarmClock alarmClock1;
+            public Form()
+            {
+                alarmClock1 = new AlarmClock();
+                alarmClock1.OnTick += PrintTick;
+                alarmClock1.OnAlarm += PrintAlarm;
+            }
+
+            //控制台输出滴答事件信息
+            void PrintTick(object sender)
+            {
+                Console.WriteLine("滴答");
+            }
+
+            //控制台输出响铃事件信息
+            void PrintAlarm(object sender)
+            {
+                Console.WriteLine("叮铃铃，叮铃铃……");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Form form1 = new Form();
+            form1.alarmClock1.Tick();//模拟闹钟滴答
+            form1.alarmClock1.Alarm();//模拟闹钟响铃
+        }
+    }
+}
