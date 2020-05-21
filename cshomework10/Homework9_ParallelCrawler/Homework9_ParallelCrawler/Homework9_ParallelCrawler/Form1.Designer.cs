dedd4a19a78ac29708c072953c90901c91b0ac99
yuwen-chen616 <57294551+yuwen-chen616@users.noreply.github.com>
2020-05-21 22:33:11 +0800

    Add files via upload
    作业：将上次作业的爬虫程序，使用并行处理进行优化，实现更快速的网页爬取。

---
+++ cshomework10/Homework9_ParallelCrawler/Homework9_ParallelCrawler/Homework9_ParallelCrawler/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace CrawlerForm {
+  partial class Form1 {
+    /// <summary>
+    /// 必需的设计器变量。
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// 清理所有正在使用的资源。
+    /// </summary>
+    /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+    protected override void Dispose(bool disposing) {
+      if (disposing && (components != null)) {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows 窗体设计器生成的代码
+
+    /// <summary>
+    /// 设计器支持所需的方法 - 不要修改
+    /// 使用代码编辑器修改此方法的内容。
+    /// </summary>
+    private void InitializeComponent() {
+            this.urlresult = new System.Windows.Forms.DataGridView();
+            this.index = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.URL = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.status = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lblInfo = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.urlresult)).BeginInit();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // urlresult
+            // 
+            this.urlresult.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.urlresult.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.urlresult.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.index,
+            this.URL,
+            this.status});
+            this.urlresult.Location = new System.Drawing.Point(33, 119);
+            this.urlresult.Margin = new System.Windows.Forms.Padding(4);
+            this.urlresult.Name = "urlresult";
+            this.urlresult.RowTemplate.Height = 23;
+            this.urlresult.Size = new System.Drawing.Size(1353, 568);
+            this.urlresult.TabIndex = 3;
+            // 
+            // index
+            // 
+            this.index.DataPropertyName = "Index";
+            this.index.HeaderText = "任务序号";
+            this.index.Name = "index";
+            // 
+            // URL
+            // 
+            this.URL.DataPropertyName = "URL";
+            this.URL.HeaderText = "URL";
+            this.URL.Name = "URL";
+            this.URL.Width = 500;
+            // 
+            // status
+            // 
+            this.status.DataPropertyName = "Status";
+            this.status.HeaderText = "状态";
+            this.status.Name = "status";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 696);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Padding = new System.Windows.Forms.Padding(1, 0, 19, 0);
+            this.statusStrip1.Size = new System.Drawing.Size(1417, 22);
+            this.statusStrip1.TabIndex = 4;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.txtUrl);
+            this.flowLayoutPanel1.Controls.Add(this.btnStart);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(4);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Padding = new System.Windows.Forms.Padding(27, 25, 0, 0);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(1417, 76);
+            this.flowLayoutPanel1.TabIndex = 5;
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Location = new System.Drawing.Point(31, 29);
+            this.txtUrl.Margin = new System.Windows.Forms.Padding(4);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(548, 25);
+            this.txtUrl.TabIndex = 4;
+            this.txtUrl.Text = "http://www.cnblogs.com/dstang2000/";
+            this.txtUrl.TextChanged += new System.EventHandler(this.txtUrl_TextChanged);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(587, 29);
+            this.btnStart.Margin = new System.Windows.Forms.Padding(4);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(100, 29);
+            this.btnStart.TabIndex = 3;
+            this.btnStart.Text = "开始";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.AutoSize = true;
+            this.lblInfo.Location = new System.Drawing.Point(31, 85);
+            this.lblInfo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(0, 15);
+            this.lblInfo.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1417, 718);
+            this.Controls.Add(this.lblInfo);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.urlresult);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.urlresult)).EndInit();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+    }
+
+    #endregion
+    private System.Windows.Forms.DataGridView urlresult;
+    private System.Windows.Forms.StatusStrip statusStrip1;
+    private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+    private System.Windows.Forms.TextBox txtUrl;
+    private System.Windows.Forms.Button btnStart;
+    private System.Windows.Forms.Label lblInfo;
+    private System.Windows.Forms.DataGridViewTextBoxColumn index;
+    private System.Windows.Forms.DataGridViewTextBoxColumn URL;
+    private System.Windows.Forms.DataGridViewTextBoxColumn status;
+  }
+}
+
---
+++ cshomework10/Homework9_ParallelCrawler/Homework9_ParallelCrawler/Homework9_ParallelCrawler/ParallelCrawler.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+//作业：将上次作业的爬虫程序，使用并行处理进行优化，实现更快速的网页爬取。
+
+namespace CrawlerForm
+{
+    internal class ParallelCrawler
+    {
+        public event Action<ParallelCrawler> CrawlerStopped;
+        public event Action<ParallelCrawler, int, string, string> PageDownloaded;
+        public string StartURL = "https://www.cnblogs.com/dstang2000/";
+        //所有URL，key是URL，value表示是否下载成功
+        private ConcurrentDictionary<string, bool> urls = new ConcurrentDictionary<string, bool>();
+        private ConcurrentQueue<string> queuepend = new ConcurrentQueue<string>();//待下载队列
+
+        public void Start()
+        {
+            urls.Clear();
+            queuepend = new ConcurrentQueue<string>();
+            queuepend.Enqueue(StartURL);
+            List<Task> tasks = new List<Task>();
+            int complatedCount = 0;//已完成的任务数
+            PageDownloaded += (crawler, index, url, info) => { complatedCount++; };
+
+            while (tasks.Count < 100)
+            {
+                if (!queuepend.TryDequeue(out string url))
+                {
+                    if (complatedCount < tasks.Count)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        break;//所有任务都完成
+                    }
+                }
+                int index = tasks.Count;
+                Task task = Task.Run(() => DownloadAndParse(url, index));
+                tasks.Add(task);
+            }
+            Task.WaitAll(tasks.ToArray()); //等待剩余任务执行完毕
+            CrawlerStopped(this);
+        }
+
+        private void DownloadAndParse(string url, int index)
+        {
+            try
+            {
+                string html = DownLoad(url, index);
+                urls[url] = true;
+                Parse(html, url);//解析,并加入新的链接
+                PageDownloaded(this, index, url, "success");
+            }
+            catch (Exception ex)
+            {
+                PageDownloaded(this, index, url, "Error:" + ex.Message);
+            }
+        }
+
+        private string DownLoad(string url, int index)
+        {
+            WebClient webClient = new WebClient();
+            webClient.Encoding = Encoding.UTF8;
+            string html = webClient.DownloadString(url);
+            File.WriteAllText(index + ".html", html, Encoding.UTF8);
+            return html;
+        }
+
+        private void Parse(string html, string pageUrl)
+        {
+            var matches = new Regex(@"(href|HREF)\s*=\s*[""'](?<url>[^""'#>]+)[""']").Matches(html);
+            foreach (Match match in matches)
+            {
+                string URL = match.Groups["url"].Value;
+                if (URL == null || URL == "") continue;
+                URL = TransferUrl(URL, pageUrl);//转绝对路径
+
+                string urlParse = @"^(?<site>https?://(?<host>[\w\d.]+)(:\d+)?($|/))([\w\d]+/)*(?<file>[^#?]*)";
+                //解析出host和file两个部分，进行过滤
+                Match linkUrlMatch = Regex.Match(URL, urlParse);
+                string host = linkUrlMatch.Groups["host"].Value;
+                string file = linkUrlMatch.Groups["file"].Value;
+                if (file == "") file = "index.html";
+
+                string HostFilter = "^" + host + "$";
+                if (Regex.IsMatch(host, HostFilter) && Regex.IsMatch(file, ".html?$")
+                  && !urls.ContainsKey(URL))
+                {
+                    queuepend.Enqueue(URL);
+                    urls.TryAdd(URL, false);
+                }
+            }
+        }
+
+
+        //将相对路径转为绝对路径
+        static private string TransferUrl(string url, string pageUrl)
+        {
+            if (url.Contains("://"))
+            {
+                return url;
+            }
+            if (url.StartsWith("//"))
+            {
+                return "http:" + url;
+            }
+            if (url.StartsWith("/"))
+            {
+                string urlParse = @"^(?<site>https?://(?<host>[\w\d.]+)(:\d+)?($|/))([\w\d]+/)*(?<file>[^#?]*)";
+                Match urlMatch = Regex.Match(pageUrl, urlParse);
+                String site = urlMatch.Groups["site"].Value;
+                return site.EndsWith("/") ? site + url.Substring(1) : site + url;
+            }
+
+            if (url.StartsWith("../"))
+            {
+                url = url.Substring(3);
+                int idx = pageUrl.LastIndexOf('/');
+                return TransferUrl(url, pageUrl.Substring(0, idx));
+            }
+
+            if (url.StartsWith("./"))
+            {
+                return TransferUrl(url.Substring(2), pageUrl);
+            }
+
+            int end = pageUrl.LastIndexOf("/");
+            return pageUrl.Substring(0, end) + "/" + url;
+        }
+    }
+}
